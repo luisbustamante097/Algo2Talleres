33df64d3530058555e403495c958412dc44a27f6
Luis Bustamante <luisbustamante091@gmail.com>
2020-02-07 20:12:54 -0300

    Agregado Taller 4

---
+++ ejercitacion_punteros/src/SistemaDeMensajes.h
@@ -0,0 +1,31 @@
+#ifndef SISTEMADEMENSAJES_H
+#define SISTEMADEMENSAJES_H
+
+#include "ConexionJugador.h"
+#include <string>
+
+#if EJ == 4 || EJ == 5
+#include "Proxy.h"
+#elif EJ == 6
+#include "Proxy2.h"
+#endif
+
+using namespace std;
+
+class SistemaDeMensajes {
+  public:
+    SistemaDeMensajes();
+    void registrarJugador(int id, string ip);
+    bool registrado(int id) const;
+
+    // Pre: registrado(id)
+    void enviarMensaje(int id, string mensaje);
+
+    // Pre: registrado(id)
+    string ipJugador(int id) const;
+
+  private:
+    ConexionJugador* _conns[4];
+};
+
+#endif
